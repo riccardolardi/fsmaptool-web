a95b96dcb8b6ab29c065b1bec975baa308b80fdb
Riccardo Lardi <riccardo.lardi@gmail.com>
2020-10-19 12:21:09 +0200

    font format

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -11,39 +11,10 @@
 <style global lang="scss">
 	@import 'breakpoint-sass/stylesheets/_breakpoint.scss';
 	@import "../var.scss";
-	@font-face {
-	  font-family: 'Ubuntu';
-	  font-weight: 400;
-	  font-style: normal;
-	  font-display: swap;
-	  src: url('/fonts/Ubuntu-Regular.ttf');
-	}
-	@font-face {
-	  font-family: 'Ubuntu-Italic';
-	  font-weight: 400;
-	  font-style: normal;
-	  font-display: swap;
-	  src: url('/fonts/Ubuntu-Italic.ttf');
-	}
-	@font-face {
-	  font-family: 'Ubuntu-Bold';
-	  font-weight: 400;
-	  font-style: normal;
-	  font-display: swap;
-	  src: url('/fonts/Ubuntu-Bold.ttf');
-	}
+  @import "../type.scss";
 	#sapper {
 		position: relative;
 		min-height: 100vh;
-	}
-	body {
-		font-family: 'Ubuntu';
-		font-size: 1.2rem;
-		line-height: 135%;
-		color: #4f9eaf;
-    @include breakpoint($breakMobile) {
-      font-size: 4vw;
-    }
 	}
 	main {
 		padding: 2rem 2rem 0;
@@ -52,49 +23,6 @@
 		color: #192e3b;
     white-space: nowrap;
 	}
-  h1 {
-    font-size: 1.2rem;
-  }
-  h2 {
-  	color: #192e3b;
-  	font-size: 2rem;
-  	line-height: 4rem;
-    max-width: 75%;
-		@include breakpoint($breakMobile) {
-			line-height: 2.4rem;
-			margin-bottom: 1rem;
-		}
-  }
-	h3 {
-		font-size: 2rem;
-		line-height: 4rem;
-		@include breakpoint($breakMobile) {
-			line-height: 2.4rem;
-		}
-	}
-  .bold {
-    font-family: 'Ubuntu-Bold';
-  }
-  .italic {
-  	font-family: 'Ubuntu-Italic';
-  }
-  .cite {
-		&:before {
-			content: "«";
-		}
-		&:after {
-			content: "»";
-		}
-  }
-  p {
-  	max-width: 65vw;
-  	@include breakpoint($breakMobile) {
-  		max-width: none;
-  	}
-	  & + p {
-	    margin-top: 1rem;
-	  }
-  }
 </style>
 
 <GoogleAnalytics {stores} id={ga_measurment_id} />
